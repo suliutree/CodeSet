6e8ec4dea3596e2fbfed01d6b6db7ff11ec89cb7
suliutree <suliutree@gmail.com>
2017-07-27 21:52:31 +0800

    callback static

---
+++ callback_static/moduleb.cpp
@@ -0,0 +1,25 @@
+// Martin Reddy
+
+#include "moduleb.h"
+
+namespace apibook {
+
+ModuleB::ModuleB() : mCallback(NULL), mClosure(NULL)
+{
+}
+
+void ModuleB::SetCallback(CallbackType cb, void *data)
+{
+	mCallback = cb;
+	mClosure = data;
+}
+
+void ModuleB::DoAction()
+{
+	if (mCallback)
+	{
+		(*mCallback)("Hello World", mClosure);
+	}
+}
+
+}
