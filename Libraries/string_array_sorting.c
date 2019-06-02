9346b761e7a89a6a4099f49763bcef186a55cf8b
suliutree <suliutree@gmail.com>
2019-06-02 18:34:53 +0800

    use c library to sort and lookup

--- Libraries/string_array_sorting.c
+++ Libraries/string_array_sorting.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* string compare of *p1 and *p2 */
 int scmp(const void *p1, const void *p2)
@@ -11,7 +12,7 @@ int scmp(const void *p1, const void *p2)
 }
 
 /* integer compare of *p1 and *p2 */
-int icmp(const void *p1, const void *p1)
+int icmp(const void *p1, const void *p2)
 {
     int v1, v2;
     v1 = *(int *)p1;
@@ -23,6 +24,27 @@ int icmp(const void *p1, const void *p1)
     else
         return 1;
 }
+
+
+typedef struct Nameval Nameval;
+struct Nameval {
+	char *name;
+	int value;
+};
+
+Nameval htmlchars[] = {
+	"AElig", 	0x00c6,
+	"Aacute", 	0x00c1,
+	"Acirc", 	0x00c2,
+	"Burice", 	0x00b5,
+	"Curle", 	0x0078,
+	"Diff",		0x0101,
+	"Egio",		0x0034,
+	"Free",		0x56b3,
+	"zeta",		0x7777,
+};
+
+int nvcmp(const void *va, const void *vb);
 
 /* use bsearch to find name in tab, return index */
 int lookup(char *name, Nameval tab[], int ntab)
@@ -46,4 +68,65 @@ int nvcmp(const void *va, const void *vb)
     a = (Nameval *)va;
     b = (Nameval *)vb;
     return strcmp(a->name, b->name);
+}
+
+int main()
+{
+    // string array sorting
+    char *str[] = {
+        "book",
+        "array",
+        "something",
+        "anything",
+        "without",
+        "change",
+        "zip",
+        "package"
+    };
+
+    int array_length = sizeof(*str) / sizeof(str[0][0]);
+
+    printf("original array: \n");
+    for (int i = 0; i < array_length; ++i)
+    {
+        printf("%s\n", str[i]);
+    }
+
+    qsort(str, array_length, sizeof(str[0]), scmp);
+
+    printf("sorted array: \n");
+    for (int i = 0; i < array_length; ++i)
+    {
+        printf("%s\n", str[i]);
+    }
+
+    // integer array sorting
+    int arr[] = {12, 34, 32, 90, 45, 65, 43, 888, 79, 234};
+    int int_length = sizeof(arr) / sizeof(arr[0]);
+
+    printf("original int array: ");
+    for (int i = 0; i < int_length; ++i)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    qsort(arr, int_length, sizeof(arr[0]), icmp);
+
+    printf("sorted int array: ");
+    for (int i = 0; i < int_length; ++i)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Nameval lookup
+    int ntab = sizeof(htmlchars) / sizeof(htmlchars[0]);
+    int index1 = lookup("Acirc", htmlchars, ntab);
+    int index2 = lookup("Nothing", htmlchars, ntab);
+    int index3 = lookup("Free", htmlchars, ntab);
+
+    printf("index1: %d, index2: %d, index3: %d", index1, index2, index3);
+
+    return 0;
 }
